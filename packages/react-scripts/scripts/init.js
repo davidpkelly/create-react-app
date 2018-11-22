360603fd9d2b825d12b6e164400996ac69b70ba6
David Kelly <dvkelly@juniper.net>
2018-11-21 22:57:09 -0800

    Updates for custom CRA react-scripts

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -219,6 +219,11 @@ module.exports = function(
 
   console.log();
   console.log(`Success! Created ${appName} at ${appPath}`);
+  console.log('====================================');
+  console.log('Juniper Networks Custom React Script');
+  console.log();
+  console.log('====================================');
+  console.log();
   console.log('Inside that directory, you can run several commands:');
   console.log();
   console.log(chalk.cyan(`  ${displayedCommand} start`));
